6145e5f00160bdfe280187d0f781d167d31e6599
unidaddigitalrcnradio <opaez@rcnradio.com.co>
2017-09-12 15:30:54 -0500

    slide noticias
    slide noticias

--- src/assets/js/actions.js
+++ src/assets/js/actions.js
@@ -1,21 +1,17 @@
-    var swiperCert = new Swiper('.certSlider', {
-        slidesPerView: 5,
+var swiperCert = new Swiper('.certSlider', {
+        slidesPerView: 3    ,
         paginationClickable: true,
         spaceBetween: 30,
         loop:true,
         autoplay: 4000,
         breakpoints: {
             768: {
-                slidesPerView: 3,
-                spaceBetween: 20
-            },
-            520: {
                 slidesPerView: 2,
-                spaceBetween: 10
-            },
-            400: {
+                spaceBetween: 20
+            }, 
+            520: {
                 slidesPerView: 1,
-                spaceBetween: 0
-            },
+                spaceBetween: 10
+            },      
         }
     }); 
